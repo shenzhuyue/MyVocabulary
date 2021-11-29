7e21b66c148fbc2a8273ab383e2e51e6effa0895
Erii <yxa1250336094@163.com>
2021-11-30 00:43:40 +0800

    2021.11.30 添加了向有道查询翻译的逻辑，并处理了下前端的显示。 已经可以成功的将查询到的信息再前端返回。 好耶

--- src/main/java/erii/erii/controller/wordController.java
+++ src/main/java/erii/erii/controller/wordController.java
@@ -1,6 +1,7 @@
 package erii.erii.controller;
 
 import erii.erii.common.Result;
+import erii.erii.common.SearchFromYoudao;
 import erii.erii.entity.word;
 import erii.erii.repo.WordRepository;
 import io.swagger.v3.oas.annotations.Operation;
@@ -11,12 +12,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.util.Map;
+
 /**
  * @Classname wordController
  * @Description TODO
  * @Date 2021/11/24 22:07
  * @Created by Administrator
  */
+//
+//        中文	zh-CHS
+//        英文	en
+//        日文	ja
 
 @CrossOrigin
 @Configuration
@@ -30,9 +38,11 @@ public class wordController {
     }
     @Operation(method = "GET",description = "查找")
     @PostMapping("/search")
-    public Result<?> search(@RequestParam(value = "ch",required = false) String ch,@RequestParam(value = "en",required = false) String en,@RequestParam(value = "jp",required = false) String jp){
-        word curWord=wordRepository.findAllByEn(en);
-        System.out.println(curWord);
-        return Result.ok(curWord);
+    public Result<?> search(@RequestBody Map<String,String> map) throws IOException {
+
+        System.out.println(map.get("en"));
+
+        String res= SearchFromYoudao.mySearch(map.get("en"),"en","zh-CHS");
+        return Result.ok(res);
     }
 }
