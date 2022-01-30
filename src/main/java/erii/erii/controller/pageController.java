04c97b7e29ee0405f311b5ab3580ba39c6ae3088
shenzhuyue <63398829+shenzhuyue@users.noreply.github.com>
2022-01-30 17:58:54 +0800

    Update pageController.java

--- src/main/java/erii/erii/controller/pageController.java
+++ src/main/java/erii/erii/controller/pageController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class pageController {
-    @RequestMapping({"/vocabulary"})
+    @RequestMapping({"/","/vocabulary"})
     public String indexVocabulary() {
         return "vocabulary";
     }
